121793a30caf3fc9853af40f37e6a72609143f66
nitipon <nitipon.apaisri@gmail.com>
2024-10-04 13:17:46 +0700

    Refactor transition duration in app.vue

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="dark:bg-black bg-white min-h-lvh">
+  <div class="dark:bg-black bg-white min-h-lvh transition-colors duration-300">
     <NuxtRouteAnnouncer />
     <NuxtPage />
   </div>
